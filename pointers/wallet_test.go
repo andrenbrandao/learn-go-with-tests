d6706a4657e3f515afe2d084857cf18bb4283939
André Brandão <andrenbrandao@gmail.com>
2023-03-12 23:08:42 -0300

    feat(pointers): implement verification of insufficient funds

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,6 +13,18 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -23,5 +35,14 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(21))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, "insufficient funds")
 	})
 }
