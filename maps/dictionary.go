ec00d9c0501ef03e420f422233cd2b8917bc9f39
André Brandão <andrenbrandao@gmail.com>
2023-07-21 19:03:38 -0300

    refactor(maps): refactor search word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,11 +4,13 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	val, ok := d[word]
 
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return val, nil
