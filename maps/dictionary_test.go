111cfb0be40d76c7fa30daa6351a1ea0660ff31a
André Brandão <andrenbrandao@gmail.com>
2023-07-21 23:32:51 -0300

    feat(maps): create method Delete

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -65,6 +65,18 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
+}
+
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
